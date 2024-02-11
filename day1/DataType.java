776dd95581a13d40dc9897e3741d0da210480bf2
BijiBenz <159673058+BijiBenz@users.noreply.github.com>
2024-02-11 08:19:49 +0530

    Add files via upload
    Day1Training

---
+++ day1/DataType.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noOfTabs = 4;
+		char logo = 'C';
+		float version = 121.02f;
+		String browserName = "Chrome";
+		boolean browserOpen = true;
+		
+		System.out.println("Number of Tabs : "+noOfTabs+"\n"+"Browser Name : " + browserName);
+
+	}
+
+}
---
+++ day1/HelloWorld.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hallo World");
+		System.out.println("This is my first code.");
+		System.out.println();
+	}
+
+}
